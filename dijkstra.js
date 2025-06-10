769bae610bd21b6b4b20c3ebfdd5b0377d2c4522
Bex Edmondson <bexedmondson@gmail.com>
2025-06-10 23:51:55 +0100

    maybe i can access dijkstra function now?

--- dijkstra.js
+++ dijkstra.js
@@ -5,8 +5,9 @@
  *
  * Dijkstra path-finding functions. Adapted from the Dijkstar Python project.
  *
- * Copyright (C) 2008
- *   Wyatt Baldwin <self@wyattbaldwin.com>
+ * Copyright (C) 2023
+ *   Bex Edmondson
+ *   original: Wyatt Baldwin <self@wyattbaldwin.com>
  *   All rights reserved
  *
  * Licensed under the MIT license.
@@ -21,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *****************************************************************************/
+
 var dijkstra = {
   single_source_shortest_paths: function(graph, s, d) {
     // Predecessor map for each node that has been encountered.
@@ -41,18 +43,16 @@ var dijkstra = {
 
     var closest,
         u, v,
-        cost_of_s_to_u,
+        u_cost,
         adjacent_nodes,
-        cost_of_e,
-        cost_of_s_to_u_plus_cost_of_e,
-        cost_of_s_to_v,
-        first_visit;
+        edge_cost,
+        v_cost;
     while (!open.empty()) {
       // In the nodes remaining in graph that have a known cost from s,
       // find the node, u, that currently has the shortest path from s.
       closest = open.pop();
       u = closest.value;
-      cost_of_s_to_u = closest.cost;
+      u_cost = closest.cost;
 
       // Get nodes adjacent to u...
       adjacent_nodes = graph[u] || {};
@@ -63,27 +63,29 @@ var dijkstra = {
       for (v in adjacent_nodes) {
         if (v in adjacent_nodes) {
           // Get the cost of the edge running from u to v.
-          cost_of_e = adjacent_nodes[v];
+          edge_cost = adjacent_nodes[v];
 
-          if (typeof(cost_of_e) !== 'number') {
-            var msg1 = ['Cost of edge from ', u, ' to ', v, ' is not a number! Type:', typeof(cost_of_e)].join('');
+          if (typeof(edge_cost) !== 'number') {
+            var msg1 = ['Cost of edge from ', u, ' to ', v, ' is not a number! Type:', typeof(edge_cost)].join('');
             throw new Error(msg1);
           }
 
           // Cost of s to u plus the cost of u to v across e--this is *a*
           // cost from s to v that may or may not be less than the current
           // known cost to v.
-          cost_of_s_to_u_plus_cost_of_e = cost_of_s_to_u + cost_of_e;
 
           // If we haven't visited v yet OR if the current known cost from s to
           // v is greater than the new cost we just found (cost of s to u plus
           // cost of u to v across e), update v's cost in the cost list and
           // update v's predecessor in the predecessor list (it's now u).
-          cost_of_s_to_v = costs[v];
-          first_visit = (typeof costs[v] === 'undefined');
-          if (first_visit || cost_of_s_to_v > cost_of_s_to_u_plus_cost_of_e) {
-            costs[v] = cost_of_s_to_u_plus_cost_of_e;
-            open.push(v, cost_of_s_to_u_plus_cost_of_e);
+          v_cost = costs[v];
+          if (typeof(costs[v]) === 'undefined' || v_cost > u_cost + edge_cost) {
+            costs[v] = u_cost + edge_cost;
+
+            if (v !== d) {
+              open.push(v, costs[v]);
+            }
+
             predecessors[v] = u;
           }
         }
@@ -163,6 +165,6 @@ var dijkstra = {
 
 
 // node.js module exports
-if (typeof module !== 'undefined') {
+//if (typeof module !== 'undefined') {
   module.exports = dijkstra;
-}
+//}
